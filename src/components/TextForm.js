0921256e4532ef89845374e1c97afcaa3284b0ff
Md Habib Al Muttakii Khan <m04khan1995@gmail.com>
2023-05-08 15:15:26 +0600

    copy text
    copy text function added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -31,6 +31,11 @@ export default function TextForm(props) {
 
     }
     const [text, setText] = useState('Enter Text Here');
+    const handleCopy = () => {
+
+        navigator.clipboard.writeText(text); 
+        props.showAlert("Copied to Clipboard!", "success");
+    }
 // text="new text"; //wrong way
 // setText("new text"); //correct way
   return (
@@ -43,13 +48,15 @@ export default function TextForm(props) {
       <button disabled={text.length===0} className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to uppercase</button>
       <button disabled={text.length===0} className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to lowercase</button>
       <button disabled={text.length===0} className="btn btn-primary mx-2" onClick={speak}>Speak</button>
+      <button disabled={text.length===0} className="btn btn-primary mx-2" onClick={handleCopy}>Copy text</button>
+
 
 
     </div>
     <div className="container my-4" style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h1>Your text here</h1>
-        <p>{text.trim().length === 0 ? 0 : text.trim().split(" ").length} words and {text.length} characters</p>
-        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} minutes read</p>
+        <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+        <p>{0.008 *  text.split(" ").filter((element)=>{return element.length!==0}).length}  minutes read</p>
         <h3>Preview</h3>
         <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
 
